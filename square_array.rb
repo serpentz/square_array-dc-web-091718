b451b9275db59e02869a9eb8605a7c99e4bbc6e0
Absalom Cherinet <absalomcherinet@gmail.com>
2018-08-30 19:34:43 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   # your code here
-  new_array = Array.new(array.size/2)
+  new_array = Array.new((array.size/2).ceil)
   array.each do |num|
 new_array.push(num*num)
   end
